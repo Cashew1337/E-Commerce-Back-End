4432533c923b4ac673e48b7ecc5ba1a629369854
Mr. Macro <mr.matt.caschetto@gmail.com>
2023-04-23 10:12:56 -0400

    commit product routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,7 +6,7 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const categories = await Category.findAll({
-      include: [{ model: Product, }]
+      include: [{ model: Product }]
     });
     res.status(200).json(categories)
   } catch (err) {
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
       include: [{ model: Product }]
     });
     if (!categoryData) {
-      res.status(404).json({ message: 'No category found with this id!' });
+      res.status(404).json({ message: 'No category found with this id.' });
       return;
     }
 
@@ -64,7 +64,7 @@ router.delete('/:id', async (req, res) => {
       },
     });
     if (!deleteCategory) {
-      res.status(404).json({ message: 'No category found with this id!' });
+      res.status(404).json({ message: 'No category found with this id.' });
       return;
     }
     res.status(200).json(deleteCategory);
